81917525df651dd88c91d611beaa417f69e2ac17
Scott Culotta <limeywop@hotmail.com>
2017-07-04 21:46:12 -0400

    First push to git issue with video search bar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from'./components/video_detail';
+const API_KEY = 'AIzaSyCTt_oLQB-zcqqc-uqsLBFE5If6q8Jg9AI';
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new compoennt. This compnent should produce
+// some HTML
+class App extends Component{
+  constructor(props) {
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('cats');
+  }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+   videoSearch(term) {
+     YTSearch({key: API_KEY, term: 'term'}, (videos) => {
+       this.setState({
+         videos: videos,
+         selectedVideo: videos[0]
+       });
+     });
+   }
+
+  render () {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos} />
+      </div>
+    );
+  }
+ }
+
+// take this component's generated html and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
